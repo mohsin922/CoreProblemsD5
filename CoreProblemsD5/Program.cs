5b714c5d3e45f38067a9968d0033b2378494110a
unknown <mohsinpeer22@gmail.com>
2021-12-18 13:41:04 +0530

    Power of 2

--- CoreProblemsD5/Program.cs
+++ CoreProblemsD5/Program.cs
@@ -1,28 +1,41 @@
 ﻿using System;
-namespace LEAPYEAR
+namespace PowerOf2
 {
 
-
-    public class LeapYear
+    public class PowersOf2
     {
 
+        public static Boolean isInputValid(int num)
+        {
+            return num >= 0 && num <= 31;
+        }
 
+        public static void powerOf2(int num)
+        {
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine("2 power " + i + " is: " + Math.Pow(2, i));
+            }
+        }
 
         public static void Main(String[] args)
         {
-            Console.WriteLine("Enter the Year : ");
-            int year = Convert.ToInt32(Console.ReadLine());
 
-            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            while (true)
             {
-                Console.WriteLine("{0} is a leap Year ", year);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a leap Year ", year);
+                Console.WriteLine("Enter the value of N: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (!isInputValid(n))
+                {
+                    Console.WriteLine("Entered number is invalid");
+                }
+                else
+                {
+                    powerOf2(n);
+                    break;
 
+                }
             }
         }
     }
-
 }
