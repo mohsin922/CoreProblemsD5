be5d4d995c86299656ed59c5c6b63e18c08521e4
unknown <mohsinpeer22@gmail.com>
2021-12-18 16:29:16 +0530

    Even or Odd

--- CoreProblemsD5/Program.cs
+++ CoreProblemsD5/Program.cs
@@ -1,32 +1,25 @@
 ﻿using System;
 namespace CoreProblems
 {
-    public class SwapTwoNum
+    public class EvenOrOdd
     {
 
-        public static void beforeSwap(int a, int b)
+        public static void check(int n)
         {
-            Console.WriteLine("Numbers before Swapping : " + a + "" + b);
-        }
-
-        public static void afterSwap(int a, int b)
-        {
-            int change;
-            change = a;
-            a = b;
-            b = change;
-            Console.WriteLine("Numbers after Swapping : " + a + "" + b);
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number " + n + " is even");
+            }
+            else
+            {
+                Console.WriteLine("The number " + n + " is odd");
+            }
         }
         public static void Main(String[] args)
         {
-
-            
-            Console.WriteLine("Enter the first number : ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number : ");
-            int b = Convert.ToInt32(Console.ReadLine());
-            beforeSwap(a, b);
-            afterSwap(a, b);
+            Console.WriteLine("Enter the number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            check(num);
         }
     }
 }
