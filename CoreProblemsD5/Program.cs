a2f1d8d033036fac3b194b128414f7a709201b23
unknown <mohsinpeer22@gmail.com>
2021-12-18 12:15:24 +0530

    Head Tail Percentage

---
+++ CoreProblemsD5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CoreProblemsD5
+{
+
+    public class PercentageOfHeadsNTails
+    {
+        
+        public static  double flipCoin()
+        {
+            Random random = new Random();
+            return random.NextDouble();
+            
+        }
+
+        public static void Main(String[] args)
+        {
+            
+            int totalDieRoll = 0;
+            int headCount = 0;
+            int tailCount = 0;
+            
+
+
+            Console.WriteLine("Enter the number of times to flip the coin : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < userInput; i++)
+            {
+                double FlipCoin = flipCoin();
+                totalDieRoll++;
+
+                if (FlipCoin <= 0.5)
+                {
+                    headCount++;
+                    Console.WriteLine("Heads");
+                }
+                else if (FlipCoin >= 0.5)
+                {
+                    tailCount++;
+                    Console.WriteLine("Tails");
+                }
+            }
+
+            double headPercentage = ((double)headCount / (double)totalDieRoll) * 100;
+            Console.WriteLine("Head Percentage : " + headPercentage);
+            double tailPercentage = ((double)tailCount / (double)totalDieRoll) * 100;
+            Console.WriteLine("Tail Percentage : " + tailPercentage);
+
+        }
+    }
+}
